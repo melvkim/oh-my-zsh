538758fb73c835f7b501aedf587af8667189fad6
Melv Kim <melvkim@gmail.com>
2014-06-05 17:19:05 -0700

    Improve git aliases

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -21,24 +21,23 @@ compdef _git gc=git-commit
 alias gc!='git commit -v --amend'
 compdef _git gc!=git-commit
 alias gca='git commit -v -a -m'
-compdef _git gc=git-commit
+compdef _git gca=git-commit
 alias gca!='git commit -v -a --amend'
 compdef _git gca!=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
 alias gcm='git checkout master'
+compdef _git gcm=git-checkout
 alias gr='git remote'
 compdef _git gr=git-remote
-alias grv='git remote -v'
-compdef _git grv=git-remote
-alias grmv='git remote rename'
-compdef _git grmv=git-remote
+alias grr='git remote rename'
+compdef _git grr=git-remote
 alias grrm='git remote remove'
 compdef _git grrm=git-remote
-alias grset='git remote set-url'
+alias grs='git remote set-url'
 compdef _git grset=git-remote
-alias grup='git remote update'
-compdef _git grset=git-remote
+alias gru='git remote update'
+compdef _git gru=git-remote
 alias grbi='git rebase -i'
 compdef _git grbi=git-rebase
 alias grbc='git rebase --continue'
@@ -54,18 +53,12 @@ compdef gcount=git
 alias gcl='git config --list'
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=10'
-compdef _git glg=git-log
-alias glgg='git log --graph --max-count=10'
-compdef _git glgg=git-log
-alias glgga='git log --graph --decorate --all'
-compdef _git glgga=git-log
-alias glo='git log --oneline --decorate --color'
-compdef _git glo=git-log
-alias glog='git log --oneline --decorate --color --graph'
-compdef _git glog=git-log
-alias gss='git status -s'
-compdef _git gss=git-status
+alias gl='git log --graph --left-right --pretty=format:"\
+%aN authored %Cred%ar%Creset %n\
+%C(yellow)%h%Creset \"%s\"%n"'
+compdef _git gl=git-log
+alias gll='git log --stat --all --decorate --color --graph --max-count=10'
+compdef _git gll=git-log
 alias ga='git add'
 compdef _git ga=git-add
 alias gm='git merge'
